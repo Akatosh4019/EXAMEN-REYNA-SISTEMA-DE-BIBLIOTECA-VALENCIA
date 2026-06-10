e84be89c5eb3ad9bef82d01fe661ec0262223689
Samuel <samuelvalencia3019@gmail.com>
2026-06-10 16:51:53 -0500

    swagger/openapi

--- ms-valencia-lib-reserva/src/main/java/pe/edu/upeu/errors/GlobalExceptionHandler.java
+++ ms-valencia-lib-reserva/src/main/java/pe/edu/upeu/errors/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package pe.edu.upeu.errors;
 
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
 
@@ -21,6 +22,12 @@ public class GlobalExceptionHandler implements ExceptionMapper<Throwable> {
         if (exception instanceof ApiException apiEx) {
             error.put("status", apiEx.getStatus());
             return Response.status(apiEx.getStatus()).entity(error).build();
+        }
+
+        if (exception instanceof WebApplicationException webEx) {
+            int status = webEx.getResponse().getStatus();
+            error.put("status", status);
+            return Response.status(status).entity(error).build();
         }
 
         error.put("status", 500);
